3e50e9057f37e7368a38a074e6f1a41fb2c2b7d8
Alexander Thaller <alexander.thaller@trivago.com>
2016-07-27 01:17:46 +0200

    Implemented parsing from folder.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,56 @@
-#![feature(plugin)]
+#![feature(custom_derive, plugin)]
 #![plugin(serde_macros)]
-#![feature(custom_derive)]
 
 extern crate serde;
 extern crate serde_yaml;
+extern crate glob;
 
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::Result;
 use std::path::Path;
 use std::collections::BTreeMap;
+use std::vec::Vec;
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
+use glob::glob;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Host {
-    id: String,
-    realm: String,
+    #[serde(default)]
     environment: String,
+    id: String,
+    ipv4: Vec<Ipv4Addr>,
+    #[serde(default)]
+    ipv6: Vec<Ipv6Addr>,
+    kernelrelease: String,
+    kernel: String,
+    os_family: String,
+    osrelease: String,
+    os: String,
+    #[serde(default)]
+    realm: String,
+    #[serde(default)]
+    roles: Vec<String>,
+    saltversion: String,
 }
 
 fn main() {
-    let filepath = Path::new("aly3-dus.yaml");
-    let data = file_to_string(filepath).unwrap();
+    let hosts: &mut BTreeMap<String, Host> = &mut BTreeMap::new();
 
-    let deserialized_map: BTreeMap<String, Host> = serde_yaml::from_str(&data).unwrap();
+    for entry in glob("./grains/*.yaml").expect("Failed to read glob pattern") {
+        let host = match entry {
+            Ok(path) => host_from_file(path.as_path()),
+            Err(_) => BTreeMap::new(),
+        };
 
-    for (host, hostdata) in &deserialized_map {
-        println!("{}: {:#?}", host, hostdata);
+        for (name, hostdata) in host {
+            hosts.insert(name, hostdata);
+        }
+    }
+
+    for (name, hostdata) in hosts {
+        println!("{}: {:#?}", name, hostdata);
     }
 }
 
@@ -35,4 +60,13 @@ fn file_to_string(filepath: &Path) -> Result<String> {
     try!(f.read_to_string(&mut s));
 
     Ok(s)
+}
+
+fn host_from_file(filepath: &Path) -> BTreeMap<String, Host> {
+    let data = file_to_string(filepath).unwrap();
+
+    match serde_yaml::from_str(&data) {
+        Ok(map) => map,
+        Err(_) => BTreeMap::<String, Host>::new(),
+    }
 }
